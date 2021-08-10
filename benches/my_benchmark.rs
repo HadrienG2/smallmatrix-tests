510a5ace1897091f7c19d4bfc163d1884c7568b1
Hadrien Grasland <hadrien.grasland@gmx.fr>
2021-08-10 18:12:57 +0200

    Start cleaning up benchmarking code

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -1,146 +1,249 @@
+#![feature(const_generics, const_evaluatable_checked)]
+
 use criterion::{
     black_box, criterion_group, criterion_main, measurement::Measurement, BenchmarkGroup,
     Criterion, Throughput,
 };
-use more_asserts::*;
+use paste::paste;
 use rand::prelude::*;
 use simd_tests::{Scalar, SquareMatrix, Vector};
 
-#[derive(Copy, Clone, Debug)]
-enum FigureOfMerit {
-    InputBytes,
-    OutputBytes,
-    Muls,
-    Adds,
+// Enable calling a function with a tuple of arguments
+trait CallWith<Tuple, Result> {
+    fn call(&self, t: Tuple) -> Result;
 }
+//
+macro_rules! impl_call_with {
+    () => {
+        impl_call_with!(
+            (),
+            (A),
+            (A, B),
+            (A, B, C),
+            (A, B, C, D)
+        );
+    };
+    (
+        $(
+            ( $( $tuple_elem:ident ),* )
+        ),*
+    ) => {
+        $(
+            impl<
+                $( $tuple_elem, )*
+                R,
+                F: Fn( $( $tuple_elem ),* ) -> R
+            > CallWith<( $( $tuple_elem, )* ), R> for F {
+                #[allow(non_snake_case)]
+                fn call(
+                    &self,
+                    ( $( $tuple_elem, )* ) : ( $( $tuple_elem, )* )
+                ) -> R {
+                    (*self)( $( $tuple_elem ),* )
+                }
+            }
+        )*
+    }
+}
+//
+impl_call_with!();
 
-fn benchmark_impl<Inputs: Clone, Output, M: Measurement>(
+// TODO: Deduplicate these functions
+fn bench_output_bytes<Inputs: Clone, Output, M: Measurement>(
     group: &mut BenchmarkGroup<M>,
-    figure_of_merit: FigureOfMerit,
-    num_muls: usize,
-    num_adds: usize,
-    mut function: impl FnMut(Inputs) -> Output,
-    inputs: Inputs,
+    method: impl CallWith<Inputs, Output>,
+    input: Inputs,
     name: &str,
 ) {
     let output_bytes = std::mem::size_of::<Output>();
-    let input_bytes = std::mem::size_of::<Inputs>();
-    match figure_of_merit {
-        FigureOfMerit::OutputBytes => {
-            assert_gt!(output_bytes, 0);
-            group.throughput(Throughput::Bytes(output_bytes as _));
-        }
-        FigureOfMerit::InputBytes => {
-            if input_bytes == 0 {
-                return;
-            } else {
-                group.throughput(Throughput::Bytes(input_bytes as _));
-            }
-        }
-        FigureOfMerit::Muls => {
-            if num_muls == 0 {
-                return;
-            } else {
-                group.throughput(Throughput::Elements(num_muls as _));
-            }
-        }
-        FigureOfMerit::Adds => {
-            if num_adds == 0 {
-                return;
-            } else {
-                group.throughput(Throughput::Elements(num_adds as _));
-            }
-        }
-    };
-    group.bench_function(name, |b| b.iter(|| function(black_box(inputs.clone()))));
-}
-
-fn criterion_benchmark(c: &mut Criterion) {
-    let mut rng = rand::thread_rng();
-    for figure_of_merit in [
-        FigureOfMerit::OutputBytes,
-        FigureOfMerit::InputBytes,
-        FigureOfMerit::Muls,
-        FigureOfMerit::Adds,
-    ] {
-        let mut group = c.benchmark_group(format!("{:?}", figure_of_merit));
-        macro_rules! benchmark {
-            ($num_muls:expr, $num_adds:expr, $function:path $(, $input:ident)*) => {
-                #[allow(unused_parens)]
-                benchmark_impl(
-                    &mut group,
-                    figure_of_merit,
-                    $num_muls,
-                    $num_adds,
-                    |($($input),*)| $function($($input),*),
-                    ($($input),*),
-                    stringify!($function)
-                );
-            };
-        }
-        macro_rules! generate_benchmarks {
-            () => { generate_benchmarks!(1, 2, 3, 4, 5, 6, 7, 8); };
-            ($($dim:literal),*) => {
-                $(
-                    // FIXME: Find a way to split this across multiple
-                    //        compilation units
-                    // FIXME: Iterate over benchmarks, then over dimensions
-
-                    let dim_idx = rng.gen_range(0..$dim);
-                    benchmark!(0, 0, Vector::<$dim>::unit, dim_idx);
-
-                    benchmark!(0, 0, SquareMatrix::<$dim>::identity);
-
-                    let mat_elems = rng.gen::<[Scalar; $dim * $dim]>().into_iter();
-                    benchmark!(0, 0, SquareMatrix::<$dim>::from_col_major_elems, mat_elems);
-
-                    let v1 = rng.gen::<Vector<$dim>>();
-                    let v2 = rng.gen::<Vector<$dim>>();
-                    benchmark!(0, 0, Vector::<$dim>::cat, v1, v2);
-
-                    let m1 = rng.gen::<SquareMatrix<$dim>>();
-                    let m2 = rng.gen::<SquareMatrix<$dim>>();
-                    benchmark!(0, 0, SquareMatrix::<$dim>::hcat, m1, m2);
-                    benchmark!(0, 0, SquareMatrix::<$dim>::vcat, m1, m2);
-
-                    // TODO: Benchmark a number of blocks
-
-                    benchmark!(0, 0, SquareMatrix::<$dim>::transpose, m1);
-
-                    // TODO: Benchmark a number of matrix powers
-
-                    benchmark!($dim, $dim-1, Vector::<$dim>::dot, v1, v2);
-                    benchmark!($dim, $dim-1, Vector::<$dim>::squared_norm, v1);
-                    benchmark!($dim, $dim-1, Vector::<$dim>::norm, v1);
-
-                    if $dim == 3 {
-                        let v3_1 = rng.gen::<Vector<3>>();
-                        let v3_2 = rng.gen::<Vector<3>>();
-                        benchmark!(6, 3, Vector::<3>::cross, v3_1, v3_2);
-                    }
-
-                    benchmark!(0, $dim - 1, SquareMatrix::<$dim>::trace, m1);
-
-                    // FIXME: Should have implementation-specific det, inverse
-                    //        methods, so we can keep number of add/mul accurate
-                    let dim_min_1_fact = (2..$dim).product::<usize>();
-                    let dim_fact = dim_min_1_fact * $dim;
-                    benchmark!(dim_fact, dim_fact - 1, SquareMatrix::<$dim>::det, m1);
-                    let num_mat_elems = $dim * $dim;
-                    benchmark!(
-                        dim_fact + num_mat_elems * dim_min_1_fact,
-                        dim_fact - 1 + num_mat_elems * (dim_min_1_fact - 1),
-                        SquareMatrix::<$dim>::inverse,
-                        m1
-                    );
-
-                    // TODO: Add more ops, those that go by traits
-                )*
-            }
-        }
-        generate_benchmarks!();
+    if output_bytes > 0 {
+        group.throughput(Throughput::Bytes(output_bytes as _));
+        group.bench_function(name, |b| b.iter(|| method.call(black_box(input.clone()))));
     }
 }
 
-criterion_group!(benches, criterion_benchmark);
-criterion_main!(benches);
+fn bench_input_bytes<Inputs: Clone, Output, M: Measurement>(
+    group: &mut BenchmarkGroup<M>,
+    method: impl CallWith<Inputs, Output>,
+    input: Inputs,
+    name: &str,
+) {
+    let input_bytes = std::mem::size_of::<Inputs>();
+    if input_bytes > 0 {
+        group.throughput(Throughput::Bytes(input_bytes as _));
+        group.bench_function(name, |b| b.iter(|| method.call(black_box(input.clone()))));
+    }
+}
+
+fn bench_ops<Inputs: Clone, Output, M: Measurement>(
+    group: &mut BenchmarkGroup<M>,
+    method: impl CallWith<Inputs, Output>,
+    input: Inputs,
+    name: &str,
+    num_ops: usize,
+) {
+    if num_ops > 0 {
+        group.throughput(Throughput::Elements(num_ops as _));
+        group.bench_function(name, |b| b.iter(|| method.call(black_box(input.clone()))));
+    }
+}
+
+trait NumOps {
+    fn num_ops(&self, dim: usize) -> usize;
+}
+//
+impl NumOps for usize {
+    fn num_ops(&self, _dim: usize) -> usize {
+        *self
+    }
+}
+//
+impl<F: Fn(usize) -> usize> NumOps for F {
+    fn num_ops(&self, dim: usize) -> usize {
+        (*self)(dim)
+    }
+}
+
+macro_rules! for_each_dim {
+    ($action:stmt) => {
+        for_each_dim!([1, 2, 3, 4, 5, 6, 7, 8], $action)
+    };
+    ([ $( $dim:literal ),* ], $action:stmt) => {
+        $(
+            {
+                const DIM: usize = $dim;
+                $action
+            }
+        )*
+    }
+}
+
+macro_rules! generate_benchmarks {
+    (
+        $(
+            ($object:ty, $method:ident, $num_muls:expr, $num_adds:expr $(, $input_generator:ident)* )
+        ),*
+        $( , )?
+    ) => {
+        paste! {
+            $(
+                fn [< $object:lower _ $method >](c: &mut Criterion) {
+                    let mut group = c.benchmark_group(format!("{}::{}", stringify!($object), stringify!($method)));
+                    for_each_dim!(
+                        #[allow(unused_parens)]
+                        bench_output_bytes(
+                            &mut group,
+                            $object::<DIM>::$method,
+                            ( $( $input_generator::<DIM>(), )* ),
+                            &format!("OutputBytes/{}D", DIM)
+                        )
+                    );
+                    for_each_dim!(
+                        #[allow(unused_parens)]
+                        bench_input_bytes(
+                            &mut group,
+                            $object::<DIM>::$method,
+                            ( $( $input_generator::<DIM>(), )* ),
+                            &format!("InputBytes/{}D", DIM)
+                        )
+                    );
+                    for_each_dim!(
+                        #[allow(unused_parens)]
+                        bench_ops(
+                            &mut group,
+                            $object::<DIM>::$method,
+                            ( $( $input_generator::<DIM>(), )* ),
+                            &format!("Muls/{}D", DIM),
+                            NumOps::num_ops(&$num_muls, DIM)
+                        )
+                    );
+                    for_each_dim!(
+                        #[allow(unused_parens)]
+                        bench_ops(
+                            &mut group,
+                            $object::<DIM>::$method,
+                            ( $( $input_generator::<DIM>(), )* ),
+                            &format!("Adds/{}D", DIM),
+                            NumOps::num_ops(&$num_adds, DIM)
+                        )
+                    );
+                }
+            )*
+            criterion_group!(benches, $( [< $object:lower _ $method >] ),*);
+            criterion_main!(benches);
+        }
+    };
+}
+
+fn coord_idx<const DIM: usize>() -> usize {
+    rand::thread_rng().gen_range(0..DIM)
+}
+
+fn mat_elems_iter<const DIM: usize>() -> impl Iterator<Item = Scalar> + Clone
+where
+    [(); DIM * DIM]: ,
+{
+    rand::thread_rng().gen::<[Scalar; DIM * DIM]>().into_iter()
+}
+
+fn vector<const DIM: usize>() -> Vector<DIM>
+where
+    [(); DIM * 1]: ,
+{
+    rand::random()
+}
+
+fn matrix<const DIM: usize>() -> SquareMatrix<DIM>
+where
+    [(); DIM * DIM]: ,
+{
+    rand::random()
+}
+
+fn factorial(x: usize) -> usize {
+    (2..x).product()
+}
+
+fn det_muls(dim: usize) -> usize {
+    factorial(dim)
+}
+
+fn det_adds(dim: usize) -> usize {
+    det_muls(dim) - 1
+}
+
+fn num_mat_elems(dim: usize) -> usize {
+    dim * dim
+}
+
+// FIXME: Find a way to split this across multiple compilation units
+generate_benchmarks!(
+    (Vector, unit, 0, 0, coord_idx),
+    (SquareMatrix, identity, 0, 0),
+    (SquareMatrix, from_col_major_elems, 0, 0, mat_elems_iter),
+    (Vector, cat, 0, 0, vector, vector),
+    (SquareMatrix, hcat, 0, 0, matrix, matrix),
+    (SquareMatrix, vcat, 0, 0, matrix, matrix),
+    // TODO: Benchmark a number of blocks
+    (SquareMatrix, transpose, 0, 0, matrix),
+    // TODO: Benchmark a number of matrix powers
+    (Vector, dot, 0, 0, vector, vector),
+    (Vector, squared_norm, 0, 0, vector),
+    (Vector, norm, 0, 0, vector),
+    // FIXME: Figure out a way to benchmark cross, which is restricted to Vector<3>
+    (SquareMatrix, trace, 0, |dim| dim - 1, matrix),
+    // FIXME: Should have implementation-specific det and inverse methods, so
+    //        we can keep the number of add/mul accurate as impl evolves
+    (SquareMatrix, det, det_muls, det_adds, matrix),
+    (
+        SquareMatrix,
+        inverse,
+        // In Cramer's rule, one computes one cofactor per matrix element, then
+        // computes the matrix determinant and divide the matrix (ergo, every
+        // matrix element) by said determinant.
+        |dim| num_mat_elems(dim) * (det_muls(dim - 1) + 1) + det_muls(dim),
+        |dim| num_mat_elems(dim) * det_adds(dim - 1) + det_adds(dim),
+        matrix
+    ),
+    // TODO: Add more ops, those that go by traits
+);
